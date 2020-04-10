c83eeaa82fcb907342e4f588ca2235f8076273ed
saeedhassansolangi <saeedhassansolangi@gmail.com>
2020-04-10 12:40:11 +0000

    done

--- server.js
+++ server.js
@@ -6,13 +6,15 @@ const PORT = process.env.PORT || 2000;
 const flag = require('country-flag-emoji');
 
 const api = {
-    key  : "42103cbe3438aff5f2c20805be56718e",
-    base : "https://api.openweathermap.org/data/2.5/"
+    key: "42103cbe3438aff5f2c20805be56718e",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
 
 
-app.set("view engine","ejs")
-app.use(bodyparser.urlencoded({extended:true}))
+app.set("view engine", "ejs")
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
 
 
 // fetch(`${api.base}weather?q=${query}&appid=${api.key}`)
@@ -32,23 +34,24 @@ app.post("/", async (req, res) => {
         .then(response => response.json())
         .then(data => {
             try {
-                 // console.log(data);
-            let kelToCal = data.main.temp - 273.15
-            let parseKelToCal = JSON.stringify(kelToCal)
-            let removeSpace = parseKelToCal.substring(0, 4)
-            let temp = removeSpace.trim()
-            let countryEmoji = flag.get(data.sys.country)
-;
-            
-          res.render("weather", { data: data  ,temps:temp, countryEmoji:countryEmoji})
-    
-} catch (err) {
-    res.send(data , "Not found")
-}
-           
-})       .catch(err => console.log(err))
+                // console.log(data);
+                let kelToCal = data.main.temp - 273.15
+                let parseKelToCal = JSON.stringify(kelToCal)
+                let removeSpace = parseKelToCal.substring(0, 4)
+                let temp = removeSpace.trim()
+                let countryEmoji = flag.get(data.sys.country);
+
+                res.render("weather", {
+                    data: data,
+                    temps: temp,
+                    countryEmoji: countryEmoji
+                })
+
+            } catch (err) {
+                res.send(data, "Not found")
+            }
+
+        }).catch(err => console.log(err))
 })
 
 app.listen(PORT, console.log(`Server is Running on The PORT ${PORT}`))
-
-
